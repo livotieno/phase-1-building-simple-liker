de72da9cf44ff9e52b9f4e534b02e480b5f06b41
livotieno <livio5261@gmail.com>
2023-10-05 09:45:17 +0300

    Done

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+//add class .hidden to error modal
+const modal = document.querySelector('#modal')
+modal.classList.add('hidden')
 
+const hearts = document.querySelectorAll(".like-glyph")
+hearts.forEach(heart => {
+  heart.addEventListener('click', (e) => {
+    if (e.target.innerHTML === FULL_HEART) {
+      e.target.innerHTML = EMPTY_HEART;
+      e.target.classList.remove('activated-heart');
+    } else {
+      handleLike(e);
+    }
+  })
+})
+
+function handleLike(e) {
+  mimicServerCall()
+  .then(resp => {
+    //console.log(resp);
+    changeHeartFill(resp, e)
+  })
+  .catch(error => {
+    modal.innerHTML = error
+  })
+  setTimeout(() => {
+    modal.classList.add('hidden')
+  }, 3000)
+}
+
+function changeHeartFill(resp, e) {
+  if (resp === 'Pretend remote server notified of action!') {
+    e.target.classList.add('activated-heart')
+    e.target.innerHTML = FULL_HEART;
+  }
+}
 
 
 
